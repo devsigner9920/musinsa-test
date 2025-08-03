b73f2f61d9ebf99efdb98bd89e23f0e805fbe083
devsigner9920 <devsigner9920@gmail.com>
2025-08-03 22:37:52 +0900

    Spring Boot 애플리케이션 통합 테스트 코드 작성

---
+++ src/test/kotlin/com/musinsa/category/CategoryApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.musinsa.category
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CategoryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+        // Spring Boot 컨텍스트가 정상적으로 로드되는지 확인
+    }
+} 
